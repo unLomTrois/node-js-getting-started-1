a5ec3d129ca9b412a5476017f9919eb4c078899c
unlomtrois <vadim.kuz02@gmail.com>
2020-11-26 14:57:34 +0300

    add postgresql

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const cool = require('cool-ascii-faces');
 const express = require('express');
 const path = require('path');
+const { Pool } = require('pg');
 const PORT = process.env.PORT || 5000;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 
 showTimes = () => {
   let result = '';
@@ -16,7 +25,19 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (_, res) => res.render('pages/index'))
-  .get('/cool', (_, res) => res.send(cool()))
-  .get('/times', (_, res) => res.send(showTimes()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .get('/times', (req, res) => res.send(showTimes()))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null };
+      res.render('pages/db', results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
