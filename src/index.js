c4d9e583c8cb8832352fd575981557f637ccd3c8
unlomtrois <vadim.kuz02@gmail.com>
2020-11-27 22:27:25 +0300

    update structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-const cool = require('cool-ascii-faces');
 const express = require('express');
-const path = require('path');
 const { Pool } = require('pg');
-const PORT = process.env.PORT || 5000;
+const path = require('path');
 
+const PORT = process.env.PORT || 5000;
+const app = express();
+const app_dir = path.join(__dirname, '..')
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
@@ -11,23 +12,11 @@ const pool = new Pool({
   }
 });
 
-
-showTimes = () => {
-  let result = '';
-  const times = process.env.TIMES || 5;
-  for (i = 0; i < times; i++) {
-    result += i + ' ';
-  }
-  return result;
-}
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
+app
+  .use(express.static(path.join(app_dir, 'public')))
+  .set('views', path.join(app_dir, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .get('/times', (req, res) => res.send(showTimes()))
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect();
@@ -40,5 +29,4 @@ express()
       res.send("Error " + err);
     }
   })
-  .get('/test', (req, res) => res.send('github test'))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
